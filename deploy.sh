e1f2bb2b52f387f63966a42b758ceb01e4f04416
Yanis Alfian <yanisalfian@gmail.com>
2020-04-03 23:24:36 +0700

    change from ssh to https

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:yanisalfian/covid19-bar-race.git master:gh-pages
+git push -f https://github.com/yanisalfian/covid19-bar-race.git master:gh-pages
 
 cd -
